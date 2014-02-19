3c70f03b5ffe59c9bcb7e5b981521ad8145ce94a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2014-02-19 19:21:54 +0100

    chore: add gulp watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 var gulp = require('gulp');
 var traceur = require('gulp-traceur');
 
+var paths = {
+  scripts: [
+    'batmobile.js',
+    'index.js',
+    'node_modules/hydrogen-boosted-engine/engine.js',
+    'node_modules/Diary.js/src/**/*.js',
+    'node_modules/di/src/*.js'
+  ]
+};
+
 gulp.task('default', function () {
-  gulp.src([
-      'batmobile.js',
-      'index.js',
-      'node_modules/hydrogen-boosted-engine/engine.js',
-      'node_modules/Diary.js/src/**/*.js',
-      'node_modules/di/src/*.js'
-    ])
+  gulp.src(paths.scripts)
     .pipe(traceur({
       types: true,
       annotations: true,
       modules: 'amd'
     }))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['default']);
 });
